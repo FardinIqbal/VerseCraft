2f75698c9dc4b983bc2580b211653281b337fd73
LemonTea333 <52869603+FardinIqbal@users.noreply.github.com>
2024-10-02 20:56:57 -0400

    refactor: Simplify routes and move featured poems to home page
    - Remove separate featured poems route
    - Update PagesController to fetch featured poems for home page
    - Update home view to display featured poems
    - Simplify routes.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @featured_poems = Poem.where(featured: true).includes(:user).order(created_at: :desc).limit(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,20 @@ Rails.application.routes.draw do
   # Authentication routes
   devise_for :users
 
-  # User profile routes
-  # Only allowing show, edit, and update actions for user profiles
-  resources :users, only: [:show, :edit, :update]
-
   # Root path (homepage)
+  # This is where featured poems will be displayed
   root 'pages#home'
+
+  # User profile routes
+  resources :users, only: [:show, :edit, :update]
 
   # Poem routes with nested routes for likes and comments
   resources :poems do
-    collection do
-      get 'featured'
-    end
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :update, :destroy]
   end
 
+  # Admin namespace
   namespace :admin do
     resources :poems, only: [:index] do
       member do
@@ -27,11 +25,8 @@ Rails.application.routes.draw do
   end
 
   # Health check route
-  # Useful for monitoring the application's status
   get 'up', to: 'rails/health#show', as: :rails_health_check
 
-  # Example of a custom route (commented out for now)
-  # get 'custom_path', to: 'controller#action', as: :custom_route_name
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see:
+  # https://guides.rubyonrails.org/routing.html
 end
