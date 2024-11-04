6a9e368d997380e8cc0d84270c6311d6f005da9b
LemonTea333 <52869603+FardinIqbal@users.noreply.github.com>
2024-11-03 21:35:37 -0500

    fix: configure production secrets and environment
    - Add new credentials and master key
    - Configure production environment
    - Set up secret key handling
    - Enable eager loading in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 # config/environments/production.rb
 Rails.application.configure do
-  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  config.require_master_key = true
+  config.eager_load = true
+  config.secret_key_base = ENV['SECRET_KEY_BASE'] || Rails.application.credentials.secret_key_base
 end
